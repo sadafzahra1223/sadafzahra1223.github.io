8c31c21624574894abfaea23b576bd886e9640a7
sadafzahra1223 <sadafzahra1223@gmail.com>
2022-07-18 04:38:25 +0100

    new directory for blog and portfolio json

---
+++ src/pages/components/post.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default function Post({ title, description, id, slug }) {
+  return (
+  
+  		
+	    <div class="card mb-4">
+	       
+	        <div class="card-body">
+	            <div class="small text-muted">January 1, 2022</div>
+	            <h2 class="card-title h4">{title}</h2>
+	            <p class="card-text" dangerouslySetInnerHTML={{ __html: description }}></p>
+	            <Link href={`/${slug}`}>
+	            	<a class="btn btn-primary" href={`/${slug}`}>Read more →</a>
+      		    </Link>
+	            
+	        </div>
+            </div>
+ 
+  )
+}
