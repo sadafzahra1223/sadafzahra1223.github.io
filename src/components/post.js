1489340137ee48447d306cbc3cdc1ff9e59a3d2c
sadafzahra1223 <sadafzahra1223@gmail.com>
2022-07-18 05:06:32 +0100

    amend Read more to view in portfolio index

--- src/components/post.js
+++ src/components/post.js
@@ -11,7 +11,7 @@ export default function Post({ title, description, id, slug }) {
 	            <h2 class="card-title h4">{title}</h2>
 	            <p class="card-text" dangerouslySetInnerHTML={{ __html: description }}></p>
 	            <Link href={`/${slug}`}>
-	            	<a class="btn btn-primary" href={`/${slug}`}>Read more →</a>
+	            	<a class="btn btn-primary" href={`/${slug}`}>View →</a>
       		    </Link>
 	            
 	        </div>
