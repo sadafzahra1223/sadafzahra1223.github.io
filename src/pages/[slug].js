cc15c89aefb64de61f3591f97fa6ad3137a29cce
sadafzahra1223 <sadafzahra1223@gmail.com>
2023-02-15 11:17:05 +1100

    social cards & patterns page

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -109,6 +109,7 @@ axios.post('https://sheetdb.io/api/v1/rw00c59mbwfd3',{
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item" id="load-svg"><a class="nav-link" href="/patterns">Patterns</a></li> 
                     <li class="nav-item" id="load-svg"><a class="nav-link" href="/">Portfolio</a></li>   
                     <li class="nav-item" id="create-svg"><a class="nav-link" href="/blog">Blog</a></li>
                       
